2f8914da3c9cc9044a2c386b63a1f088344b35ae
Zachary <zcolwell@asu.edu>
2017-12-03 13:43:42 -0700

    First real code for ORB related stuff. We livin'

--- app/src/main/jni/native-lib.cpp
+++ app/src/main/jni/native-lib.cpp
@@ -4,6 +4,7 @@
 #include "opencv2/core.hpp"
 
 using namespace cv;
+using namespace std;
 
 extern "C" {
 
@@ -21,7 +22,33 @@ JNIEXPORT jstring JNICALL
 Java_asuforia_group2_asuforia_ASUForia_nativeFeatureDetection(JNIEnv *env, jobject This, jobject referenceImage) {
 
     // TODO: Implement nativeFeatureDetection to extract ORB features from reference image
+
+
+
+
     
+    Mat img = imread("C:\\Users\\Zachary\\AndroidStudioProjects\\eee598-asuforiaFINAL-effinger-colwell\\app\\src\\main\\res\\drawable");
+    Mat descriptors_object;
+    Mat descriptors_scene;
+
+    Ptr<FeatureDetector> detector = ORB::create();
+    Ptr<DescriptorExtractor> extractor = ORB::create();
+    vector<KeyPoint> kp;
+    vector<KeyPoint> kp2;
+
+    detector->detect(img,kp);
+    extractor->compute(img,kp,descriptors_object);
+    extractor->compute(img,kp2,descriptors_scene);
+
+    FlannBasedMatcher matcher;
+
+    vector<vector<DMatch>>matches;
+
+    matcher.knnMatch(descriptors_object,descriptors_scene, matches,2);
+
+
+
+
 
 
     return env->NewStringUTF("");
