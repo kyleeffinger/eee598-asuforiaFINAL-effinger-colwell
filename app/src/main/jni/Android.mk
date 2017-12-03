ce2688748dc6524f5c1c03b1fa2925985bb8e826
Zachary <zcolwell@asu.edu>
2017-12-03 13:38:38 -0700

    Merge branch 'master' of C:\Users\Zachary\AndroidStudioProjects\eee598-asuforiaFINAL-effinger-colwell with conflicts.

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,20 +2,19 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-OPENCV_ROOT:=/Users/kyleeffinger/OpenCV-android-sdk
+OPENCV_ROOT:=C:\Users\Zachary\Desktop\OpenCV-android-sdk
 OPENCV_CAMERA_MODULES:=on
 OPENCV_INSTALL_MODULES:=on
 OPENCV_LIB_TYPE:=SHARED
 include ${OPENCV_ROOT}/sdk/native/jni/OpenCV.mk
 
-NDK_MODULE_PATH=/Users/kyleeffinger/Library/Android/sdk/ndk-bundle
+NDK_MODULE_PATH=C:\Users\Zachary\AppData\Local\Android\Sdk
 LOCAL_ARM_NEON := true
 LOCAL_SRC_FILES := native-lib.cpp
 LOCAL_CPPFLAGS := -std=gnu++0x
 LOCAL_CFLAGS += -O2
 LOCAL_LDLIBS += -llog -ldl
 LOCAL_MODULE := group2
-
 
 include $(BUILD_SHARED_LIBRARY)
 
