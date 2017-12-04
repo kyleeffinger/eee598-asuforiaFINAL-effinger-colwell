752c202559364732bd23a2df6921548887b0e801
Zachary <zcolwell@asu.edu>
2017-12-03 18:12:56 -0700

    Code updated to what was emailed to Robert

--- app/src/main/jni/native-lib.cpp
+++ app/src/main/jni/native-lib.cpp
@@ -31,19 +31,30 @@ Java_asuforia_group2_asuforia_ASUForia_nativeFeatureDetection(JNIEnv *env, jobje
     // TODO: Implement nativeFeatureDetection to extract ORB features from reference image
 
     //Read the reference image
-    Mat img = imread("C:\\Users\\Zachary\\AndroidStudioProjects\\eee598-asuforiaFINAL-effinger-colwell\\app\\src\\main\\res\\drawable",CV_LOAD_IMAGE_GRAYSCALE);
+    Mat img  = imread("C:\\Users\\Zachary\\AndroidStudioProjects\\eee598-asuforiaFINAL-effinger-colwell\\app\\src\\main\\res\\drawable\\referenceimage.png",CV_LOAD_IMAGE_GRAYSCALE);
+    Mat img2 = imread("C:\\Users\\Zachary\\AndroidStudioProjects\\eee598-asuforiaFINAL-effinger-colwell\\app\\src\\main\\res\\drawable\\referenceimage2.png",CV_LOAD_IMAGE_GRAYSCALE);
     Mat outputImage;
 
-//    if(&img != NULL)
-//        __android_log_print(ANDROID_LOG_INFO, "Mat", "Image Loaded");
-//    else
-//        __android_log_print(ANDROID_LOG_INFO, "Mat", "Image NOT Loaded");
+
+    if(&img != NULL)
+        __android_log_print(ANDROID_LOG_INFO, "Mat", "Image Loaded");
+    else
+        __android_log_print(ANDROID_LOG_INFO, "Mat", "Image NOT Loaded");
+
+    if(&img2 != NULL)
+        __android_log_print(ANDROID_LOG_INFO, "Mat", "Image Loaded");
+    else
+        __android_log_print(ANDROID_LOG_INFO, "Mat", "Image NOT Loaded");
 
 
-     //cvtColor(img,grayImg,CV_BGR2GRAY,0);
-    //FEATURE DETECTION & EXTRACTION
-    Mat descriptors1;
-    Mat descriptors2;
+    //FEATURE DESCRIPTORS IMAGE 1
+    Mat descriptors11;
+    Mat descriptors12;
+
+    //FEATURE DESCRIPTORS IMAGE 2
+    Mat descriptors21;
+    Mat descriptors22;
+
 
     //Ptr<FeatureDetector> detector = ORB::create();
     Ptr<Feature2D> detector = ORB::create();
@@ -53,15 +64,15 @@ Java_asuforia_group2_asuforia_ASUForia_nativeFeatureDetection(JNIEnv *env, jobje
 
     detector->detect(img,kp);
 
-    extractor->compute(img,kp,descriptors1);
-    extractor->compute(img,kp2,descriptors2);
+    extractor->compute(img,kp,descriptors11);
+    extractor->compute(img,kp2,descriptors12);
 
     //FEATURE MATCHING
     FlannBasedMatcher matcher;
 
     vector<vector<DMatch>>matches;
 
-    matcher.knnMatch(descriptors1,descriptors2, matches,2);
+    matcher.knnMatch(img,img2, matches,2);
 
 
     return env->NewStringUTF("");
